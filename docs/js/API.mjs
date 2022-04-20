968576322bb1c1dc8440ec63e657dd689904420b
Dan Tiberi <danny.tiberi@gmail.com>
2022-04-20 12:02:26 -0500

    User login api call.

--- docs/js/API.mjs
+++ docs/js/API.mjs
@@ -1,6 +1,8 @@
 export default class API {
 
-    constructor() {}
+    constructor() {
+        this.cloudUrl = "https://itmd-467-fp-web-app-express.azurewebsites.net";
+    }
 
     /**
      * Example function for an async fetch request to a given url.
@@ -14,4 +16,29 @@ export default class API {
                 .then(data => resolve(data));
         });
     }
+
+    /**
+     * Returns the user's app_user row upon successful login. Stores this data in session storage under the key "userData".
+     * Returns false if the login is invalid. 
+     * @param {*} email 
+     * @param {*} password 
+     */
+    async userLogin(email, password){
+        return new Promise((resolve, reject) => {
+            fetch(this.cloudUrl+`/userLogin?email=${email}&password=${password}`)
+                .then(response => response.json())
+                .then(data => {
+                    if(data.hasOwnProperty('error'))//Invalid login.
+                    {
+                        console.log(data.error);
+                        return false;
+                    }
+                    else{
+                        sessionStorage.setItem('userData', data);
+                        resolve(data);
+                    }
+                });
+        });
+    }
 }
+
