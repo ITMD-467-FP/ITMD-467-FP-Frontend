7033abea30949d4d8e614441a21bb319b269b193
Dan Tiberi <danny.tiberi@gmail.com>
2022-04-20 11:20:50 -0500

    Created class for testing API.mjs as development continues.

---
+++ docs/js/testAPI.mjs
@@ -0,0 +1,6 @@
+console.log("Test API!");
+//This class is for testing the class in API.mjs along side development. It also serves as an example for other developers to follow. - Dan Tiberi
+
+import API from './API.mjs'
+
+var api = new API();
