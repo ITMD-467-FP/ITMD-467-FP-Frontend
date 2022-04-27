f9a57d998d0ae433e20dcc6000de2d6cac47bb9e
Dan Tiberi <danny.tiberi@gmail.com>
2022-04-26 21:57:23 -0500

    Update trends.mjs

--- docs/js/trends.mjs
+++ docs/js/trends.mjs
@@ -2,11 +2,18 @@ import API from './API.mjs'
 
 var api = new API();
 
-function beginTrendLoad(){
-    /*
-    - Grab trendsHold
-    - Call findTrends in api.
-    - Create html element for each trend.
-    - Add those elements to trendsHold.
-    */
-}
+console.log("trends.mjs");
+
+/*
+- Grab trendsHold
+- Call findTrends in api.
+- Create html element for each trend.
+- Add those elements to trendsHold.
+*/
+
+(async () => {
+    const output = await api.findTrends(JSON.parse(sessionStorage.getItem('userData')).id);
+    console.log(output);
+    //resolve(output);
+})();
+
